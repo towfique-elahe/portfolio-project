82098cc794f22f929f4d601edc8afbe44df53d93
Towfique Elahe <towfiqueelahe6@gmail.com>
2022-09-30 00:19:34 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// dropdown button starts
 function dropDown() {
     document.getElementById("menuContent").classList.toggle("show");
 }
+
+window.onclick = function (event) {
+    if (!event.target.matches('.menu-button')) {
+        var dropdowns = document.getElementsByClassName("menu-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+// dropdown button ends
